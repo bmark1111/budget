fb696ecbac7f6b381bef1e61c839814a663a94ac
markhamb <markhamb@localhost>
2015-11-26 10:41:42 -0800

    Change to show "budget_views" before current and "budget-views - 1" after current
    Also show Forecast as separate from Actual

--- app/scripts/filters/getIntervals.js
+++ app/scripts/filters/getIntervals.js
@@ -1,11 +1,13 @@
 app.filter('getIntervals', function ($localStorage) {
 
-	return function (intervals, start_interval) {
+	return function (intervals, start_interval, colspan) {
 		var return_intervals = Array();
 		angular.forEach(intervals,
 			function(interval, index) {
-				if (index >= start_interval && return_intervals.length < $localStorage.budget_views) {
-					return_intervals.push(interval);
+				if (index >= start_interval && return_intervals.length < ($localStorage.budget_views * 2)) {
+					if (typeof colspan === 'undefined' || (index&colspan) !== 0) {
+						return_intervals.push(interval);
+					}
 				}
 			})
 		return return_intervals;
