9247bb2206df1c162c9de7bff1c052ab9773a6a4
markhamb <markhamb@localhost>
2015-11-29 06:56:54 -0800

    Move forward and backwards through budget

--- app/scripts/controllers/budgetController.js
+++ app/scripts/controllers/budgetController.js
@@ -1,6 +1,8 @@
 'use strict';
 
-app.controller('BudgetController', ['$q', '$scope', '$rootScope', 'RestData2', '$filter', 'Categories', function($q, $scope, $rootScope, RestData2, $filter, Categories) {
+app.controller('BudgetController', ['$q', '$scope', '$rootScope', '$localStorage', 'RestData2', '$filter', 'Categories',
+
+function($q, $scope, $rootScope, $localStorage, RestData2, $filter, Categories) {
 
 	$scope.dataErrorMsg = [];
 	$scope.dataErrorMsgThese = false;
@@ -68,33 +70,6 @@ app.controller('BudgetController', ['$q', '$scope', '$rootScope', 'RestData2', '
 		// load the intervals
 		if (!!response[1].success) {
 			buildPeriods(response[1]);
-//			// set current interval
-//			$rootScope.intervals = [];
-//			$rootScope.start_interval = 0;
-//			angular.forEach(response[1].data.result,
-//				function(interval, key) {
-//					var sd = new Date(new Date(interval.interval_beginning).setHours(0,0,0,0));
-//					var ed = new Date(new Date(interval.interval_ending).setHours(0,0,0,0));
-//					var now = new Date(new Date().setHours(0,0,0,0));
-//					if (+now >= +sd && +now <= +ed) {
-//						interval.alt_ending = now;				// set alternative ending
-//						interval.current_interval = true;		// mark the current interval
-//					}
-//
-//					angular.forEach(interval.accounts,
-//						function(account) {
-//							if (account.reconciled_date) {
-//								var dt = account.reconciled_date.split('-');
-//								var rd = new Date(dt[0], --dt[1], dt[2]);
-//								if (+rd === +ed) {
-//									// if everything has been reconciled up to the period ending date
-//									account.reconciled = 1;
-//								}
-//							}
-//						});
-//
-//					$rootScope.intervals[key] = interval;
-//				});
 		}
 	});
 
@@ -124,83 +99,84 @@ app.controller('BudgetController', ['$q', '$scope', '$rootScope', 'RestData2', '
 	$scope.moveInterval = function(direction) {
 		interval = interval + direction;
 
-		if (direction == -1) {
+		if (direction === -1) {
 			if ($rootScope.start_interval > 0) {
 				// move the start pointer
-				$rootScope.start_interval--;
+				$rootScope.start_interval -= 2;
 			} else {
 				// add an array element at the beginning
-				getNext(0, direction);
+				getNext(-1);
 			}
-		} else if (direction == 1) {
-			$rootScope.start_interval++;
-			var last_interval = $rootScope.start_interval + 11;
-			if (typeof($rootScope.intervals[last_interval]) == 'undefined') {
-				getNext($rootScope.intervals.length - 1, direction);
+		} else if (direction === 1) {
+			$rootScope.start_interval += 2;
+			var last_interval = $rootScope.start_interval + $localStorage.budget_views;
+			if (typeof($rootScope.intervals[last_interval]) === 'undefined') {
+				getNext(1);
 			}
 		}
 	};
 
-	var getNext = function(index, direction) {
-		RestData2().getTransactions(
-				{
-					interval: interval
-				},
-				function(response) {
-					if (!!response.success) {
-						var moved = Array();
-						// if moving backwards add interval to front of array
-						if (direction == -1) {
-							moved.push(response.data.result[0]);
-						}
-						// add the current intervals
-						angular.forEach($rootScope.intervals,
-							function(interval) {
-								moved.push(interval)
-							});
-						// if moving forward add interval to end of array
-						if (direction == 1) {
-							// make adjustment to the account balances
-							angular.forEach(response.data.result[0].accounts,
-								function(account, index) {
-									if (typeof(response.data.result[0].adjustments[account.bank_account_id]) !== 'undefined') {
-										account.balance = parseFloat(moved[moved.length-1].accounts[index].balance) + parseFloat(response.data.result[0].adjustments[account.bank_account_id]);
-									} else {
-										account.balance = parseFloat(moved[moved.length-1].accounts[index].balance)
-									}
-//console.log("------- account.bank_account_id = "+account.bank_account_id+" ------")
-//console.log("prev balances = "+moved[moved.length-1].balances[account.bank_account_id]);
-//console.log("this balances = "+response.data.result[0].balances[account.bank_account_id]);
-									if (typeof(moved[moved.length-1].balances) !== 'undefined' && typeof(response.data.result[0].balances) !== 'undefined') {
-										var prev_account_balance = (typeof(moved[moved.length-1].balances[account.bank_account_id]) !== 'undefined') ? moved[moved.length-1].balances[account.bank_account_id]: 0;
-										var this_account_balance = (typeof(response.data.result[0].balances[account.bank_account_id]) !== 'undefined') ? response.data.result[0].balances[account.bank_account_id]: 0;
-//console.log("prev_account_balances = " + prev_account_balance);
-//console.log("this_account_balances = " + this_account_balance);
-//console.log("account.balance = "+account.balance);
-										if (parseFloat(this_account_balance) > parseFloat(prev_account_balance)) {
-//console.log('+++++++++')
-											account.balance += (parseFloat(this_account_balance) - parseFloat(prev_account_balance));
-										}
-//console.log("account.balance = "+account.balance);
-									}
-								});
-							response.data.result[0].balance_forward = moved[moved.length-1].running_total;
-							response.data.result[0].running_total = response.data.result[0].balance_forward + response.data.result[0].interval_total;
-							moved.push(response.data.result[0]);
-						}
-						$rootScope.intervals = moved;
-					} else {
-						if (response.errors) {
-							angular.forEach(response.errors,
-								function(error) {
-									$scope.dataErrorMsg.push(error.error);
-								})
-						} else {
-							$scope.dataErrorMsg[0] = response;
-						}
+	var getNext = function(direction) {
+		RestData2().getTransactions({
+				interval: interval
+			},
+			function(response) {
+				if (!!response.success) {
+					var moved = Array();
+					// if moving backwards add interval to front of array
+					if (direction == -1) {
+						moved.push(response.data.result[0]);	// add forecast
+						moved.push(response.data.result[1]);	// add actual
 					}
+					// add the current intervals
+					angular.forEach($rootScope.intervals,
+						function(interval) {
+							moved.push(interval)
+						});
+					// if moving forward add interval to end of array
+					if (direction == 1) {
+//						// make adjustment to the account balances
+//						angular.forEach(response.data.result[0].accounts,
+//							function(account, index) {
+//								if (typeof(response.data.result[0].adjustments[account.bank_account_id]) !== 'undefined') {
+//									account.balance = parseFloat(moved[moved.length-1].accounts[index].balance) + parseFloat(response.data.result[0].adjustments[account.bank_account_id]);
+//								} else {
+//									account.balance = parseFloat(moved[moved.length-1].accounts[index].balance)
+//								}
+////console.log("------- account.bank_account_id = "+account.bank_account_id+" ------")
+////console.log("prev balances = "+moved[moved.length-1].balances[account.bank_account_id]);
+////console.log("this balances = "+response.data.result[0].balances[account.bank_account_id]);
+//								if (typeof(moved[moved.length-1].balances) !== 'undefined' && typeof(response.data.result[0].balances) !== 'undefined') {
+//									var prev_account_balance = (typeof(moved[moved.length-1].balances[account.bank_account_id]) !== 'undefined') ? moved[moved.length-1].balances[account.bank_account_id]: 0;
+//									var this_account_balance = (typeof(response.data.result[0].balances[account.bank_account_id]) !== 'undefined') ? response.data.result[0].balances[account.bank_account_id]: 0;
+////console.log("prev_account_balances = " + prev_account_balance);
+////console.log("this_account_balances = " + this_account_balance);
+////console.log("account.balance = "+account.balance);
+//									if (parseFloat(this_account_balance) > parseFloat(prev_account_balance)) {
+////console.log('+++++++++')
+//										account.balance += (parseFloat(this_account_balance) - parseFloat(prev_account_balance));
+//									}
+////console.log("account.balance = "+account.balance);
+//								}
+//							});
+//						response.data.result[0].balance_forward = moved[moved.length-1].running_total;
+//						response.data.result[0].running_total = response.data.result[0].balance_forward + response.data.result[0].interval_total;
+						moved.push(response.data.result[0]);	// add forecast
+						moved.push(response.data.result[1]);	// add actual
+					}
+					$rootScope.intervals = moved;
+				} else {
+					if (response.errors) {
+						angular.forEach(response.errors,
+							function(error) {
+								$scope.dataErrorMsg.push(error.error);
+							})
+					} else {
+						$scope.dataErrorMsg[0] = response;
+					}
+				}
 //					ngProgress.complete();
-				});
+			});
 	}
 
 }]);
--- app/scripts/filters/getIntervals.js
+++ app/scripts/filters/getIntervals.js
@@ -2,9 +2,10 @@ app.filter('getIntervals', function ($localStorage) {
 
 	return function (intervals, start_interval, colspan) {
 		var return_intervals = Array();
+		var budget_views = (typeof colspan === 'undefined') ? $localStorage.budget_views: parseInt($localStorage.budget_views/2);
 		angular.forEach(intervals,
 			function(interval, index) {
-				if (index >= start_interval && return_intervals.length < ($localStorage.budget_views * 2)) {
+				if (index >= start_interval && return_intervals.length <= budget_views) {
 					if (typeof colspan === 'undefined' || (index&colspan) !== 0) {
 						return_intervals.push(interval);
 					}
