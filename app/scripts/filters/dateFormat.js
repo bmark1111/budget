8c706217341d838d7ad63a04b2496753f1787da1
markhamb <markhamb@localhost>
2016-05-02 22:18:12 -0700

    Fix period ending date in sheet and budget

--- app/scripts/filters/dateFormat.js
+++ app/scripts/filters/dateFormat.js
@@ -1,15 +1,16 @@
 app.filter('displayDate', function ($localStorage, $filter) {
 
 	return function (input) {
+		var dt = input.split('T');
 		switch ($localStorage.budget_mode) {
 			case 'weekly':
-				return $filter('date')(input, "EEE MMM dd, yyyy");
+				return $filter('date')(dt[0], "EEE MMM dd, yyyy");
 			case 'bi-weekly':
-				return $filter('date')(input, "EEE MMM dd, yyyy");
+				return $filter('date')(dt[0], "EEE MMM dd, yyyy");
 			case 'semi-monthly':
-				return $filter('date')(input, "EEE MMM dd, yyyy");
+				return $filter('date')(dt[0], "EEE MMM dd, yyyy");
 			case 'monthly':
-				return $filter('date')(input, "MMM yyyy");
+				return $filter('date')(dt[0], "MMM yyyy");
 		}
 	};
 });
